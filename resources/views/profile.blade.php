de423e49b3e28f1c8231a38aa336b87867584305
AhmadAriefMaulana <ahm.ariefmaulana@gmail.com>
2024-07-10 14:02:17 +0800

    views update as usual part 2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -17,7 +17,7 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
               <li class="breadcrumb-item active">Profile</li>
             </ol>
           </div><!-- /.col -->
@@ -31,17 +31,17 @@
       <div class="content">
         <div class="card">
           <div class="firstinfo">
-            <img src="{{ asset ('lte/dist/img/me.jpg') }}">
+            {{-- <img src="{{ asset ('lte/dist/img/me.jpg') }}"> --}}
             <div class="profileinfo">
                <h1>Runa </h1>
                <h3>Peternak Ayam</h3>
                <p class="bio">
-                 Lived all my life on the top of mount Fuji, learning the way to be a Ninja Dev.
+                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa error animi quia debitis facere voluptatibus odio esse
                </p>
             </div>
           </div>
         </div>
-        <div class="badgescard">
+        {{-- <div class="badgescard">
           <span class="devicons devicons-django"></span>
           <span class="devicons devicons-python"></span>
           <span class="devicons devicons-codepen"></span>
@@ -49,7 +49,7 @@
           <span class="devicons devicons-gulp"></span>
           <span class="devicons devicons-angular"></span>
           <span class="devicons devicons-sass"></span>
-        </div>
+        </div> --}}
       </div>
       
     </section>
